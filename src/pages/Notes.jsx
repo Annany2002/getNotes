e1e37f9033244ed398867f6a20ec6dc46e073b35
Annany Vishwakarma <kingvansh009@gmail.com>
2023-04-05 12:36:11 +0530

    made some changes

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -19,7 +19,7 @@ const Notes = ({dark, setDark, notes}) => {
    useEffect(handleSearch, [text])
 
    return (
-   	<div className = 'flex my-2 rounded-xl mx-auto w-full flex-wrap lg:w-2/3 bg-myYellow items-center max-w-[600px] dark:bg-[] h-full min-h-screen'>
+   	<div className ='flex my-0 rounded-xl mx-auto w-full flex-wrap lg:w-2/3 bg-myYellow items-center max-w-[600px] dark:bg-[] h-full min-h-screen xl:my-2'>
           <div className='flex flex-row py-6 px-4 w-full justify-between'>
              {!showSearch && <h2 className='text-5xl text-[#291720] font-bold tracking-wide'>My Notes</h2>}
              {showSearch && <input className='text-gray-300 border-2 border-black outline-none rounded-xl bg-transparent focus:bg-[#291720] tracking-wide font-medium px-4' value={text} placeholder = 'Keyword...' autoFocus type="text" onChange={(e) => {setText(e.target.value);handleSearch();}}/>}
@@ -27,12 +27,10 @@ const Notes = ({dark, setDark, notes}) => {
                 {!showSearch ? <MagnifyingGlass size={32} weight='bold'/> : <X size={32} weight='bold'/>}
              </button>
           </div>
+          {filteredNotes.length == 0 && <div className='mt-3 w-full text-center bg-red-400'> <p className='text-4xl font-mono font-bold'>No Notes found...</p></div>}
           <div className='grid grid-cols-2 w-full'>
-          {filteredNotes.length == 0 && <div className='w-full bg-red-400'> <p className='text-4xl font-mono font-bold'>No Notes found...</p></div>}
-             {
-                filteredNotes.map(note => <NoteItem key={note.id} note={note}
-               />)
-             }
+             {filteredNotes.map(note => <NoteItem key={note.id} note={note}
+               />)}
           </div> 
           <div className='flex w-full justify-between'>
              <button className='px-4' onClick={() => setDark(!dark)}>{!dark ? <Moon size={38} /> : <Sun size={32}/>}</button>
